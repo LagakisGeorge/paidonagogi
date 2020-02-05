ed66b96c415ccaf3b5d6ac6aa5bb5d2c8327b132
User <glagakis@gmail.com>
2020-02-06 01:04:54 +0200

    ΣΤΟ ΦΟΡΜ2 ΜΠΟΡΩ ΝΑ ΠΕΡΝΩ ΘΕΡΑΠΟΝΤΕΣ

---
+++ PAIDONAGOGH/Form2.Designer.vb
@@ -0,0 +1,205 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form2
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form2))
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.BindingSource1 = New System.Windows.Forms.BindingSource(Me.components)
+        Me.BindingNavigator1 = New System.Windows.Forms.BindingNavigator(Me.components)
+        Me.BindingNavigatorCountItem = New System.Windows.Forms.ToolStripLabel()
+        Me.BindingNavigatorMovePreviousItem = New System.Windows.Forms.ToolStripButton()
+        Me.BindingNavigatorSeparator = New System.Windows.Forms.ToolStripSeparator()
+        Me.BindingNavigatorSeparator1 = New System.Windows.Forms.ToolStripSeparator()
+        Me.ToolStripSeparator4 = New System.Windows.Forms.ToolStripSeparator()
+        Me.BindingNavigatorPositionItem = New System.Windows.Forms.ToolStripTextBox()
+        Me.ToolStripSeparator1 = New System.Windows.Forms.ToolStripSeparator()
+        Me.ToolStripSeparator2 = New System.Windows.Forms.ToolStripSeparator()
+        Me.ToolStripSeparator3 = New System.Windows.Forms.ToolStripSeparator()
+        Me.BindingNavigatorMoveNextItem = New System.Windows.Forms.ToolStripButton()
+        Me.ToolStripButton1 = New System.Windows.Forms.ToolStripButton()
+        Me.GridView1 = New System.Windows.Forms.DataGridView()
+        Me.Button2 = New System.Windows.Forms.Button()
+        CType(Me.BindingSource1, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.BindingNavigator1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.BindingNavigator1.SuspendLayout()
+        CType(Me.GridView1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(176, 40)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(64, 45)
+        Me.Button1.TabIndex = 0
+        Me.Button1.Text = "Button1"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'BindingNavigator1
+        '
+        Me.BindingNavigator1.AddNewItem = Nothing
+        Me.BindingNavigator1.BindingSource = Me.BindingSource1
+        Me.BindingNavigator1.CountItem = Me.BindingNavigatorCountItem
+        Me.BindingNavigator1.CountItemFormat = "από {0}"
+        Me.BindingNavigator1.DeleteItem = Nothing
+        Me.BindingNavigator1.Dock = System.Windows.Forms.DockStyle.Bottom
+        Me.BindingNavigator1.Items.AddRange(New System.Windows.Forms.ToolStripItem() {Me.BindingNavigatorMovePreviousItem, Me.BindingNavigatorSeparator, Me.BindingNavigatorSeparator1, Me.ToolStripSeparator4, Me.BindingNavigatorPositionItem, Me.BindingNavigatorCountItem, Me.ToolStripSeparator1, Me.ToolStripSeparator2, Me.ToolStripSeparator3, Me.BindingNavigatorMoveNextItem, Me.ToolStripButton1})
+        Me.BindingNavigator1.Location = New System.Drawing.Point(0, 500)
+        Me.BindingNavigator1.MoveFirstItem = Nothing
+        Me.BindingNavigator1.MoveLastItem = Nothing
+        Me.BindingNavigator1.MoveNextItem = Me.BindingNavigatorMoveNextItem
+        Me.BindingNavigator1.MovePreviousItem = Me.BindingNavigatorMovePreviousItem
+        Me.BindingNavigator1.Name = "BindingNavigator1"
+        Me.BindingNavigator1.PositionItem = Me.BindingNavigatorPositionItem
+        Me.BindingNavigator1.Size = New System.Drawing.Size(872, 25)
+        Me.BindingNavigator1.TabIndex = 2
+        Me.BindingNavigator1.Text = "BindingNavigator1"
+        '
+        'BindingNavigatorCountItem
+        '
+        Me.BindingNavigatorCountItem.Name = "BindingNavigatorCountItem"
+        Me.BindingNavigatorCountItem.Size = New System.Drawing.Size(46, 22)
+        Me.BindingNavigatorCountItem.Text = "από {0}"
+        Me.BindingNavigatorCountItem.ToolTipText = "Total number of items"
+        '
+        'BindingNavigatorMovePreviousItem
+        '
+        Me.BindingNavigatorMovePreviousItem.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image
+        Me.BindingNavigatorMovePreviousItem.Image = CType(resources.GetObject("BindingNavigatorMovePreviousItem.Image"), System.Drawing.Image)
+        Me.BindingNavigatorMovePreviousItem.Name = "BindingNavigatorMovePreviousItem"
+        Me.BindingNavigatorMovePreviousItem.RightToLeftAutoMirrorImage = True
+        Me.BindingNavigatorMovePreviousItem.Size = New System.Drawing.Size(23, 22)
+        Me.BindingNavigatorMovePreviousItem.Text = "Move previous"
+        '
+        'BindingNavigatorSeparator
+        '
+        Me.BindingNavigatorSeparator.Name = "BindingNavigatorSeparator"
+        Me.BindingNavigatorSeparator.Size = New System.Drawing.Size(6, 25)
+        '
+        'BindingNavigatorSeparator1
+        '
+        Me.BindingNavigatorSeparator1.Name = "BindingNavigatorSeparator1"
+        Me.BindingNavigatorSeparator1.Size = New System.Drawing.Size(6, 25)
+        '
+        'ToolStripSeparator4
+        '
+        Me.ToolStripSeparator4.Name = "ToolStripSeparator4"
+        Me.ToolStripSeparator4.Size = New System.Drawing.Size(6, 25)
+        '
+        'BindingNavigatorPositionItem
+        '
+        Me.BindingNavigatorPositionItem.AccessibleName = "Position"
+        Me.BindingNavigatorPositionItem.AutoSize = False
+        Me.BindingNavigatorPositionItem.Name = "BindingNavigatorPositionItem"
+        Me.BindingNavigatorPositionItem.Size = New System.Drawing.Size(50, 23)
+        Me.BindingNavigatorPositionItem.Text = "0"
+        Me.BindingNavigatorPositionItem.ToolTipText = "Current position"
+        '
+        'ToolStripSeparator1
+        '
+        Me.ToolStripSeparator1.Name = "ToolStripSeparator1"
+        Me.ToolStripSeparator1.Size = New System.Drawing.Size(6, 25)
+        '
+        'ToolStripSeparator2
+        '
+        Me.ToolStripSeparator2.Name = "ToolStripSeparator2"
+        Me.ToolStripSeparator2.Size = New System.Drawing.Size(6, 25)
+        '
+        'ToolStripSeparator3
+        '
+        Me.ToolStripSeparator3.Name = "ToolStripSeparator3"
+        Me.ToolStripSeparator3.Size = New System.Drawing.Size(6, 25)
+        '
+        'BindingNavigatorMoveNextItem
+        '
+        Me.BindingNavigatorMoveNextItem.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image
+        Me.BindingNavigatorMoveNextItem.Image = CType(resources.GetObject("BindingNavigatorMoveNextItem.Image"), System.Drawing.Image)
+        Me.BindingNavigatorMoveNextItem.Name = "BindingNavigatorMoveNextItem"
+        Me.BindingNavigatorMoveNextItem.RightToLeftAutoMirrorImage = True
+        Me.BindingNavigatorMoveNextItem.Size = New System.Drawing.Size(23, 22)
+        Me.BindingNavigatorMoveNextItem.Text = "Move next"
+        '
+        'ToolStripButton1
+        '
+        Me.ToolStripButton1.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image
+        Me.ToolStripButton1.Image = CType(resources.GetObject("ToolStripButton1.Image"), System.Drawing.Image)
+        Me.ToolStripButton1.ImageTransparentColor = System.Drawing.Color.Magenta
+        Me.ToolStripButton1.Name = "ToolStripButton1"
+        Me.ToolStripButton1.Size = New System.Drawing.Size(23, 22)
+        Me.ToolStripButton1.Text = "ToolStripButton1"
+        '
+        'GridView1
+        '
+        Me.GridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        Me.GridView1.Location = New System.Drawing.Point(26, 118)
+        Me.GridView1.Name = "GridView1"
+        Me.GridView1.Size = New System.Drawing.Size(747, 292)
+        Me.GridView1.TabIndex = 3
+        '
+        'Button2
+        '
+        Me.Button2.Location = New System.Drawing.Point(346, 55)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(75, 23)
+        Me.Button2.TabIndex = 4
+        Me.Button2.Text = "Button2"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'Form2
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(872, 525)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.GridView1)
+        Me.Controls.Add(Me.BindingNavigator1)
+        Me.Controls.Add(Me.Button1)
+        Me.Name = "Form2"
+        Me.Text = "Form2"
+        CType(Me.BindingSource1, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.BindingNavigator1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.BindingNavigator1.ResumeLayout(False)
+        Me.BindingNavigator1.PerformLayout()
+        CType(Me.GridView1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Button1 As Button
+    Friend WithEvents BindingSource1 As BindingSource
+    Friend WithEvents BindingNavigator1 As BindingNavigator
+    Friend WithEvents BindingNavigatorCountItem As ToolStripLabel
+    Friend WithEvents BindingNavigatorMovePreviousItem As ToolStripButton
+    Friend WithEvents BindingNavigatorSeparator As ToolStripSeparator
+    Friend WithEvents BindingNavigatorSeparator1 As ToolStripSeparator
+    Friend WithEvents ToolStripSeparator4 As ToolStripSeparator
+    Friend WithEvents BindingNavigatorPositionItem As ToolStripTextBox
+    Friend WithEvents ToolStripSeparator1 As ToolStripSeparator
+    Friend WithEvents ToolStripSeparator2 As ToolStripSeparator
+    Friend WithEvents ToolStripSeparator3 As ToolStripSeparator
+    Friend WithEvents BindingNavigatorMoveNextItem As ToolStripButton
+    Friend WithEvents ToolStripButton1 As ToolStripButton
+    Friend WithEvents GridView1 As DataGridView
+    Friend WithEvents Button2 As Button
+End Class
---
+++ PAIDONAGOGH/Form2.vb
@@ -0,0 +1,133 @@
+﻿Imports System.Data.SqlClient
+Imports System.Data.OleDb
+
+Public Class Form2
+
+
+    'Create connection
+    Dim conn As OleDbConnection
+
+    'create data adapter
+    Dim da As OleDbDataAdapter ' SqlDataAdapter
+
+    'create dataset
+    Dim ds As DataSet = New DataSet
+
+    'Set up connection string
+    Dim cnString As String
+    'Public Property PLATOS() As Integer
+    '    Get
+    '        Return VALUE
+    '    End Get
+    '    Set(ByVal value As Integer)
+
+    '    End Set
+    'End Property
+
+    Private STHLH_ID As Integer = 1
+    Public Property STHLHTOY_ID() As Integer
+        Get
+            Return STHLH_ID
+        End Get
+        Set(ByVal Value As Integer)
+            If Value < 1 Or Value > 12 Then
+                ' Error processing for invalid value. 
+            Else
+                STHLH_ID = Value
+            End If
+        End Set
+    End Property
+
+
+
+
+
+    Private Sub cmdAdd_Click(ByVal sender As System.Object, ByVal e As System.EventArgs)
+        ' FormAdd("FrmUNIT_MEASURE_ADD")
+    End Sub
+
+    'Private Sub cmdCancel_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles cmdCancel.Click
+    '    Try
+    '        da.Update(ds, "PEL")
+    '    Catch ex As Exception
+    '        MsgBox("δεν αποθηκευτηκε" + ex.Message)
+    '    End Try
+
+    '    Me.Close()
+    'End Sub
+
+    Private Sub FrmUNIT_MEASURE_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Load
+        '  sqlSTR = "SELECT * FROM TBL_Unit_Measure"
+        ' FillListView(ExecuteSQLQuery(sqlSTR), lstunit, 0)
+        paint_ergasies()
+    End Sub
+
+
+
+    Sub paint_ergasies()
+
+        cnString = gConnect ' "Data Source=localhost\SQLEXPRESS;Integrated Security=True;database=thermo"
+        'Str_Connection = cnString
+        Dim SQLqry
+        SQLqry = "SELECT TOP 20 EPO,ID FROM THERAP " ' ORDER BY HME "
+        'conn = New SqlConnection(cnString)
+
+        Dim conn As New OleDbConnection
+        conn.ConnectionString = gConnect
+        conn.Open()
+
+
+
+        Try
+            ' Open connection
+            'conn.Open()
+
+            da = New OleDbDataAdapter(SQLqry, conn)
+
+            'create command builder
+            Dim cb As OleDbCommandBuilder = New OleDbCommandBuilder(da)
+            ds.Clear()
+            'fill dataset
+            da.Fill(ds, "PEL")
+            GridView1.ClearSelection()
+            GridView1.DataSource = ds
+            GridView1.DataMember = "PEL"
+
+            'GridView1.Columns(STHLHTOY_ID).Width = 0
+            GridView1.Columns(STHLHTOY_ID).Visible = False
+
+        Catch ex As SqlException
+            MsgBox(ex.ToString)
+        Finally
+            ' Close connection
+            conn.Close()
+        End Try
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        Try
+            da.Update(ds, "PEL")
+        Catch ex As Exception
+            MsgBox("δεν αποθηκευτηκε" + ex.Message)
+        End Try
+
+        Me.Close()
+    End Sub
+    ' Private Sub cmdEdit_Click(ByVal sender As System.Object, ByVal e As System.EventArgs)
+    ' FormEdit("FrmUNIT_MEASURE")
+    'End Sub
+
+    'Private Sub cmdDelete_Click(ByVal sender As System.Object, ByVal e As System.EventArgs)
+    'If lstunit.Items.Count > 0 Then
+    '    If MsgBox("Are you sure to delete this Unit Measure ?", MsgBoxStyle.YesNo + MsgBoxStyle.Information, "Sales and Inventory") = MsgBoxResult.Yes Then
+    '        sqlSTR = "DELETE FROM TBL_Unit_Measure WHERE ID=" & lstunit.FocusedItem.Text
+    '        ExecuteSQLQuery(sqlSTR)
+    '        sqlSTR = "SELECT * FROM TBL_Unit_Measure"
+    '        FillListView(ExecuteSQLQuery(sqlSTR), lstunit, 0)
+    '    End If
+    'End If
+    ''End Sub
+
+
+
+End Class
